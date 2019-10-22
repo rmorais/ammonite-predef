351bc947ba4345f67b0cab1b6152b1d8d672ec8a
Rui Morais <rui.morais@pm.me>
2019-10-22 22:16:12 +0100

    preload fs2

--- scripts/amm_2.12.sh
+++ scripts/amm_2.12.sh
@@ -9,6 +9,7 @@ test -e ~/.coursier/coursier || ( \
   com.lihaoyi:ammonite_2.12.10:1.7.1 \
   org.typelevel:cats-core_2.12:2.0.0 \
   org.typelevel:cats-effect_2.12:2.0.0 \
+  co.fs2:fs2-core_2.12:2.0.1 \
   -- --predef-code "\
         interp.configureCompiler(_.settings.YpartialUnification.value = true);\
       " < /dev/tty
